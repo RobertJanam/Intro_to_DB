f4e32d2d82930faa4a93865b0144cc7acfc9bf38
RobertJanam <robertbjanam@gmail.com>
2025-12-14 22:43:47 +0300

    if this doesn't work

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
-    order_id INT AUTO_INCREMENT BIGINTPRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     CONSTRAINT fk_customer_order FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
     order_date DATE DEFAULT(CURRENT_DATE)
