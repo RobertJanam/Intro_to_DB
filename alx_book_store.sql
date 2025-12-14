a096037ded1555489699332c7aa62c8fc8b95c91
RobertJanam <robertbjanam@gmail.com>
2025-12-14 22:38:49 +0300

    added a lot of things

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,38 +1,38 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store
+CREATE DATABASE IF NOT EXISTS alx_book_store USE alx_book_store;
 
 CREATE TABLE IF NOT EXISTS Books (
-    book_id INT PRIMARY KEY,
-    title VARCHAR(130),
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
     author_id INT,
-    CONSTRAINT fk_Authors FOREIGN KEY (author_id) REFERENCES Authors (author_id),
-    price DOUBLE,
+    CONSTRAINT fk_books_author FOREIGN KEY (author_id) REFERENCES Authors (author_id),
+    price DOUBLE NOT NULL CHECK (price >= 0),
     publication_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215)
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
-    order_id INT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT BIGINTPRIMARY KEY,
     customer_id INT,
-    CONSTRAINT fk_Customers FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
-    order_date DATE
+    CONSTRAINT fk_customer_order FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+    order_date DATE DEFAULT(CURRENT_DATE)
 )
 
 CREATE TABLE IF NOT EXISTS Order_Details (
-    orderdetailid INT PRIMARY KEY,
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
-    CONSTRAINT fk_Orders FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+    CONSTRAINT fk_orderdetails_order FOREIGN KEY (order_id) REFERENCES Orders (order_id),
     book_id INT,
-    FOREIGN KEY (book_id) REFERENCES Books (book_id),
-    quantity DOUBLE
+    CONSTRAINT fk_orderdetails_book FOREIGN KEY (book_id) REFERENCES Books (book_id),
+    quantity DOUBLE NOT NULL CHECK (quantity > 0)
 )
